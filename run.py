6bf55ae3759ee83c9e5a4a4d7aa50b417e6d4a83
Sergey Bakaleynik <bilaserg@gmail.com>
2019-09-28 18:35:26 +0300

    new data, changes in parser

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
-import pandas as pd
-import numpy as np
-import json
 import os
-from pymongo import MongoClient
-import pprint
+import re
 
-cwd = os.getcwd()
-files = {"2016": [], "2017": [], "2018": []}
+import pandas as pd
+
 regions = ['Российская Федерация', 'Центральный федеральный округ', 'Белгородская область', 'Брянская область',
            'Владимирская область', 'Воронежская область', 'Ивановская область', 'Калужская область',
            'Костромская область', 'Курская область', 'Липецкая область', 'Московская область', 'Орловская область',
@@ -31,47 +27,33 @@ regions = ['Российская Федерация', 'Центральный ф
            'Дальневосточный федеральный округ', 'Республика Саха (Якутия)', 'Камчатский край', 'Приморский край',
            'Хабаровский край', 'Амурская область', 'Магаданская область', 'Сахалинская область',
            'Еврейская автономная область', 'Чукотский авт.округ', 'Крымский федеральный округ', 'Республика Крым',
-           'г Севастополь']
-# client = MongoClient("mongodb://mongo_root:mongo_root@cluster0-shard-00-00-lvewy.mongodb.net:27017,"
-#                      "cluster0-shard-00-01-lvewy.mongodb.net:27017,cluster0-shard-00-02-lvewy."
-#                      "mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&"
-#                      "retryWrites=true&w=majority")
-# db = client.perinatal
-# table = db.statiscs
-for r, d, f in os.walk(cwd):
+           'г Севастополь', 'Российская Федерация с учетом Республики Крым и г. Севастополя1)', 'Республика Крым2)',
+           'г. Севастополь2)']
+
+
+def main_statics(files):
+    for year in files:
+        for i, file_path in enumerate(files[year]):
+            df = pd.read_excel(file_path, sheet_name='t1_1')
+            df.columns = ['region', 'birth', 'd1', 'd2', 'd3', 'death', 'd4', 'd5', 'd6', 'death_children', 'd7', 'd8',
+                          'd9', 'inc_decr', 'd10', 'marriage', 'd11', 'd12', 'd13', 'divorce', 'd14', 'd15', 'd16']
+            df = df[df.region.isin(regions)]
+            df = df[['region', 'birth', 'death_children', 'marriage', 'divorce']]
+            df.insert(5, "year", year)
+            month = f'0{i + 1}' if i < 9 else str(i + 1)
+            df.insert(6, "month", month)
+            file = open(f'data/main/{year}/{month}.csv', mode='w')
+            file.close()
+            df.to_csv(f'data/main/{year}/{month}.csv')
+
+
+cwd = os.getcwd()
+files = {f"{y}": [] for y in range(2012, 2019)}
+for r, d, f in os.walk(os.path.join(cwd, 'row_data')):
     for file in f:
         if '.xls' in file and '~' not in file:
             path = os.path.join(r, file)
-            if "2016" in r:
-                files["2016"].append(path)
-            elif "2017" in r:
-                files["2017"].append(path)
-            else:
-                files["2018"].append(path)
-for year in files:
-    for i, file_path in enumerate(files[year]):
-        print()
-        print(file_path)
-        df = pd.read_excel(file_path, sheet_name='t1_1')
-        df.columns = ['region', 'birth', 'd1', 'd2', 'd3', 'death', 'd4', 'd5', 'd6', 'death_children', 'd7', 'd8',
-                      'd9', 'inc_decr', 'd10', 'marriage', 'd11', 'd12', 'd13', 'divorce', 'd14', 'd15', 'd16']
-        df = df[df.region.isin(regions)]
-        df = df[['region', 'birth', 'death', 'death_children', 'inc_decr', 'marriage', 'divorce']]
-        df.insert(7, "year", year)
-        month = f'0{i + 1}' if i < 9 else str(i + 1)
-        df.insert(8, "month", month)
-        file = open(f'data/{year}/{month}.csv', mode='w')
-        file.close()
-        df.to_csv(f'data/{year}/{month}.csv')
-        # records = list(json.loads(df.T.to_json()).values())
-        # table.insert_many(records)
-
-# res_year = table.find(projection={"year": 1}).distinct("year")
-# year = [str(post) for post in res_year]
-# res_month = table.find(projection={"month": 1}).distinct("month")
-# month = [str(post) for post in res_month]
-# res_region = table.find(projection={"region": 1}).distinct("region")
-# region = [str(post) for post in res_region]
-# print(year)
-# print(month)
-# print(len(region), region)
+            year = re.findall(r'\d{4}', file)[0]
+            if year in files.keys():
+                files[year].append(path)
+main_statics(files)
